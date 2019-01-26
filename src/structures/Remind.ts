028c74d0f5f4f819550262bbcc6fe9727909c261
kiru <kiru@waifu.club>
2019-01-26 19:51:01 +0100

    var name

--- src/structures/Remind.ts
+++ src/structures/Remind.ts
@@ -36,8 +36,8 @@ export class Remind {
 	}
 
 	public async _check() {
-		const remindersRepo = this.database.db.getRepository(Reminder);
-		const reminders = await remindersRepo.find({ triggers_at: LessThan(new Date(Date.now() + this.checkRate)) });
+		const repo = this.database.db.getRepository(Reminder);
+		const reminders = await repo.find({ triggers_at: LessThan(new Date(Date.now() + this.checkRate)) });
 		const now = new Date();
 
 		for (const reminder of reminders) {
@@ -53,7 +53,7 @@ export class Remind {
 
 	public async runReminder(reminder: Reminder) {
 		try {
-			const reason = reminder.reason || `${reminder.channel ? 'y' : 'Y'}ou wanted me to remind you around this time!`;
+			const reason = reminder.reason || `${reminder.channel ? 'y' : 'Y'}ou asked me to remind you this:`;
 			const content = `${reminder.channel ? `<@${reminder.user}>, ` : ''} ${reason}\n\n<${reminder.trigger}>`;
 			const channel = reminder.channel && this.discord.cli.getChannel(reminder.channel) as TextChannel;
 
@@ -75,14 +75,14 @@ export class Remind {
 	}
 
 	public async addReminder(reminder: Reminder) {
-		const remindersRepo = this.database.db.getRepository(Reminder);
-		const rmd = new Reminder();
-		rmd.user = reminder.user;
-		if (reminder.channel) rmd.channel = reminder.channel;
-		rmd.reason = reminder.reason;
-		rmd.trigger = reminder.trigger;
-		rmd.triggers_at = reminder.triggers_at;
-		const dbReminder = await remindersRepo.save(rmd);
+		const repo = this.database.db.getRepository(Reminder);
+		const remind = new Reminder();
+		remind.user = reminder.user;
+		if (reminder.channel) remind.channel = reminder.channel;
+		remind.reason = reminder.reason;
+		remind.trigger = reminder.trigger;
+		remind.triggers_at = reminder.triggers_at;
+		const dbReminder = await repo.save(remind);
 		if (dbReminder.triggers_at.getTime() < (Date.now() + this.checkRate)) {
 			this.queueReminder(dbReminder);
 		}
