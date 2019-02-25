5115f54f46886be6b7ae4378b8957d86d761d0d0
kiru <kiru@waifu.club>
2019-02-25 12:11:09 +0100

    introduce lavaqueue

--- src/Config.ts
+++ src/Config.ts
@@ -4,7 +4,12 @@ import { ConfigBuilder, ConfigDefinitionType } from '@ayana/bento';
 export enum Config {
 	BOT_TOKEN = 'botToken',
 	BOT_PREFIX = 'botPrefix',
-	DB = 'postgres://user:password@localhost/db'
+	BOT_ID = 'botID',
+	DB = 'postgres://user:password@localhost/db',
+	REDIS = 'localhost',
+	LAVALINK_REST = 'lavalinkRest',
+	LAVALINK_WS = 'lavalinkWS',
+	LAVALINK_PASS = 'lavalinkPass'
 }
 
 export const Definitions = new ConfigBuilder()
@@ -17,8 +22,28 @@ export const Definitions = new ConfigBuilder()
 	type: ConfigDefinitionType.STRING,
 	env: 'BOT_PREFIX',
 })
+.add(Config.BOT_ID, {
+	type: ConfigDefinitionType.STRING,
+	env: 'BOT_ID',
+})
 .add(Config.DB, {
 	type: ConfigDefinitionType.STRING,
 	env: 'DB',
+})
+.add(Config.REDIS, {
+	type: ConfigDefinitionType.STRING,
+	env: 'REDIS',
+})
+.add(Config.LAVALINK_REST, {
+	type: ConfigDefinitionType.STRING,
+	env: 'LAVALINK_REST',
+})
+.add(Config.LAVALINK_WS, {
+	type: ConfigDefinitionType.STRING,
+	env: 'LAVALINK_WS',
+})
+.add(Config.LAVALINK_REST, {
+	type: ConfigDefinitionType.STRING,
+	env: 'LAVALINK_REST',
 })
 .build();
--- src/components/Discord.ts
+++ src/components/Discord.ts
@@ -9,6 +9,8 @@ import {
 } from '@ayana/bento';
 
 import { Logger } from '@ayana/logger';
+import { Client as MusicClient } from 'lavaqueue';
+import Storage from 'rejects';
 import { Config } from '../Config';
 import { DiscordEvent } from '../Constants';
 import { SettingProvider } from '../structures/Setting';
@@ -23,8 +25,46 @@ export class Discord {
 	@Variable({ type: VariableDefinitionType.STRING, name: Config.BOT_TOKEN, default: null })
 	private token: string = null;
 
+	@Variable({ type: VariableDefinitionType.STRING, name: Config.LAVALINK_WS, default: null })
+	private ws: string = null;
+
+	@Variable({ type: VariableDefinitionType.STRING, name: Config.LAVALINK_REST, default: null })
+	private rest: string = null;
+
+	@Variable({ type: VariableDefinitionType.STRING, name: Config.LAVALINK_PASS, default: null })
+	private pass: string = null;
+
+	@Variable({ type: VariableDefinitionType.STRING, name: Config.REDIS, default: null })
+	private redis: string = null;
+
+	@Variable({ type: VariableDefinitionType.STRING, name: Config.BOT_ID, default: null })
+	private id: string = null;
+
 	@Inject(SettingProvider)
 	public settingsprovider: SettingProvider;
+
+	public music: MusicClient = new MusicClient({
+		password: this.pass,
+		userID: this.id,
+		hosts: {
+			rest: `http://${this.rest}`,
+			ws: `ws://${this.ws}`,
+			redis: this.redis ? {
+				port: 6379,
+				host: this.redis,
+				db: 0
+			} : undefined
+		},
+		send: async (guild: string, packet: any): Promise<void> => {
+			if (this.cli.guilds.has(guild)) {
+				await (this as any).ws.send(packet);
+			}
+		}
+	});
+
+	public redis_music = this.music.queues.redis;
+
+	public storage = new Storage(this.redis_music);
 
 	public async onLoad() {
 		if (this.token == null) throw new Error(`Please set the BOT_TOKEN env variable to your token`);
